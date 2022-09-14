f82dd14429e39daea7e503cf5c610bec23fd8cbb
George Ehrman <georgeehrman@Georges-MacBook-Air-2.local>
2022-09-14 01:30:22 -0700

    Add basic component and simple assets.

---
+++ src/Main.tsx
@@ -0,0 +1,16 @@
+export default function Main() {
+    return (
+        <div>
+            <div>
+                Item
+            </div>
+            <div>
+                Input
+                <input type='text'>
+                    
+                </input>
+            </div>
+        </div>
+        
+    )
+}
